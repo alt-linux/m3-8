5c8367d77f4765b7108787c8c4d0374cb3f85c75
alt-linux <amogyous@gmail.com>
2026-02-26 19:55:42 +0300

    Add files via upload

--- m3-br-srv-8.sh
+++ m3-br-srv-8.sh
@@ -1,9 +1,4 @@
-cp /down/hosts /etc/ansible/
-echo "nameserver 192.168.0.1" >> /etc/resolv.conf
+echo 'cp /down/hosts /etc/ansible/ && echo "nameserver 192.168.0.1" >> /etc/resolv.conf && mount /dev/sr0 /mnt && cp /down/get_hostname_address.yml1 /etc/ansible/get_hostname_address.yml && mkdir /etc/ansible/PC-INFO/ && chmod u+rwx /etc/ansible/get_hostname_address.yml'
 echo "ansible all -m ping"
 echo "esli vse krasnoe to podkl 4erez ssh po imenam - sshuser@hq-srv, user@hq-cli, net_admin@hq-rtr, net_admin@br-rtr"
-echo "mount /dev/sr0 /mnt"
-cp /down/get_hostname_address.yml1 /etc/ansible/get_hostname_address.yml
-echo "mkdir /etc/ansible/PC-INFO/"
-echo "chmod x+rwx /etc/ansible/get_hostname_address.yml"
 echo "Proverka=ansible-playbook /etc/ansible/get_hostname_address.yml"
